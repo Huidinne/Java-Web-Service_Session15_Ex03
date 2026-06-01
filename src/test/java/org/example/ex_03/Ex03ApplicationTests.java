04e210d56118ae522a7191662bc0c6d3fe9ae78e
Huidin <quanghuy432006@gmail.com>
2026-06-01 14:29:38 +0700

    done

---
+++ src/test/java/org/example/ex_03/Ex03ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.ex_03;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ex03ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
